e9249d20c5212367fbc01dd5e62f2bec3ef5abc5
Paweł Cembaluk <241574@student.pwr.edu.pl>
2022-04-18 14:01:02 +0200

    Add autoscaling.

--- chapter2/web_server_cluster/main.tf
+++ chapter2/web_server_cluster/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
   admin_username                  = "exampleadmin"
   admin_password                  = var.vm_password
   disable_password_authentication = false # Use password auth instead of SSH keys
-  instances                       = 2 # Default number of instances // TODO: Ignore this parameter for autoscaling
+  instances                       = 2 # Default number of instances
   location                        = azurerm_resource_group.example.location
   name                            = "example-linux-virtual-machine-scale-set"
   resource_group_name             = azurerm_resource_group.example.name
@@ -42,6 +42,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
 
     ip_configuration {
       name                                   = "internal"
+      primary                                = true
       subnet_id                              = azurerm_subnet.example.id
       load_balancer_backend_address_pool_ids = [
         azurerm_lb_backend_address_pool.example.id
@@ -68,11 +69,17 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
   # For some reason this currently does not enable self-healing
   # Question: https://docs.microsoft.com/en-us/answers/questions/815029/cannot-enable-autorepair-for-vm-scale-set.html
   automatic_instance_repair {
-    enabled = true
+    enabled      = true
     grace_period = "PT10M"
   }
 
   health_probe_id = azurerm_lb_probe.example.id # Healthcheck used to determine if instance is healthy
+
+  lifecycle {
+    ignore_changes = [
+      instances # Ignore instances count and let autoscaling control it after the initial deployment
+    ]
+  }
 }
 
 # Configure a subnet
@@ -132,7 +139,7 @@ resource "azurerm_lb_rule" "example" {
   backend_address_pool_ids       = [
     azurerm_lb_backend_address_pool.example.id
   ]
-  probe_id = azurerm_lb_probe.example.id # Healthcheck used to determine if instance is healthy
+  probe_id                       = azurerm_lb_probe.example.id # Healthcheck used to determine if instance is healthy
 }
 
 # Configure a healthcheck probe
@@ -140,11 +147,74 @@ resource "azurerm_lb_rule" "example" {
 # This section is optional and in fact not needed to run the cluster.
 # Its purpose is to provide a self-healing mechanism.
 resource "azurerm_lb_probe" "example" {
-  loadbalancer_id     = azurerm_lb.example.id
-  name                = "example-lb-probe"
-  port                = 8080
-  protocol            = "Http"
-  request_path        = "/"
+  loadbalancer_id = azurerm_lb.example.id
+  name            = "example-lb-probe"
+  port            = 8080
+  protocol        = "Http"
+  request_path    = "/"
+}
+
+# Configure autoscaling
+#
+# The configuration below scales based on total incoming bytes to easily test
+# the scaling functionality. To increase the load manually, a simple command
+# can be used:
+# for n in {1..1000}; do curl http://<load balancer IP>; done
+resource "azurerm_monitor_autoscale_setting" "example" {
+  location            = azurerm_resource_group.example.location
+  name                = "example-monitor-autoscale-setting"
+  resource_group_name = azurerm_resource_group.example.name
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.example.id
+
+  profile {
+    name = "default"
+
+    capacity {
+      default = 2
+      maximum = 10
+      minimum = 1
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Network In Total" # Measure total incoming bytes
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.example.id
+        operator           = "GreaterThan"
+        statistic          = "Max" # Measure max total incoming bytes between VMs
+        threshold          = 200000 # Trigger when the metric is over 100KB
+        time_aggregation   = "Maximum" # Measure max total incoming bytes between time grains
+        time_grain         = "PT1M" # Single measurement timeframe
+        time_window        = "PT5M" # Measurement history length, in this case it will keep 5 time grains
+      }
+
+      scale_action {
+        cooldown  = "PT5M" # How long should the rule wait before being applied again?
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = 1
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Network In Total"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.example.id
+        operator           = "LessThan"
+        statistic          = "Max"
+        threshold          = 100000
+        time_aggregation   = "Maximum"
+        time_grain         = "PT1M"
+        time_window        = "PT5M"
+      }
+
+      scale_action {
+        cooldown  = "PT5M"
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = 1
+      }
+    }
+  }
 }
 
 # Output variables printed to the console after apply
@@ -152,5 +222,3 @@ output "lb_public_ip" {
   description = "Public IP address of the load balancer"
   value       = azurerm_public_ip.example.ip_address
 }
-
-// TODO: Autoscaling
