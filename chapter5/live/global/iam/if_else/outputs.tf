adf582cca466b900cfcf1334dcc0d4f6458cde5f
Paweł Cembaluk <241574@student.pwr.edu.pl>
2022-05-01 13:45:03 +0200

    Add ternary operator example.

--- chapter5/live/global/iam/if_else/outputs.tf
+++ chapter5/live/global/iam/if_else/outputs.tf
@@ -1,3 +1,7 @@
 output "if_else_directive" {
   value = "Hello, %{ if var.name != "" }${var.name}%{ else }(unnamed)%{ endif }"
 }
+
+output "ternary_operator" {
+  value = "Hello, ${ var.name != "" ? var.name : "(unnamed)"}"
+}
