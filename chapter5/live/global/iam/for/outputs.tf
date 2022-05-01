12dcd6a763cf31f079f3627e12573fe55f929921
Paweł Cembaluk <241574@student.pwr.edu.pl>
2022-05-01 13:43:13 +0200

    Add loop and if examples.

---
+++ chapter5/live/global/iam/for/outputs.tf
@@ -0,0 +1,31 @@
+output "upper_names" {
+  value = [for name in var.names : upper(name)]
+}
+
+output "short_upper_names" {
+  value = [for name in var.names : upper(name) if length(name) < 5]
+}
+
+output "bios" {
+  value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
+}
+
+output "upper_roles" {
+  value = {for name, role in var.hero_thousand_faces : upper(name) => upper(role)}
+}
+
+output "for_directive" {
+  value = <<EOF
+%{ for name in var.names }
+  ${name}
+%{ endfor }
+EOF
+}
+
+output "for_directive_strip_marker" {
+  value = <<EOF
+%{~ for name in var.names }
+  ${name}
+%{~ endfor }
+EOF
+}
